5d39ad7eb65d38294567c6862ae96a74ed50b995
Mauricio flores <mafs1893@gmail.com>
2023-09-10 22:37:57 -0400

    Configuracion del MENU MOBILE

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toogleDesktopMenu);
+menuHamIcon.addEventListener('click',toogleMobileMenu);
 
 function toogleDesktopMenu(){
-    console.log('CLICK');
+    
     desktopMenu.classList.toggle('innactive');
 }
-console.log("JS FUNCIONANDO")
+
+
+function toogleMobileMenu(){
+    
+    mobileMenu.classList.toggle('innactive');
+}
